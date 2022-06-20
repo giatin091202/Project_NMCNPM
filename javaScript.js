aa903e90926f9055742803184690521e15042384
giatin091202 <81601756+giatin091202@users.noreply.github.com>
2022-06-20 19:38:42 +0700

    Add files via upload

---
+++ javaScript.js
@@ -0,0 +1,20 @@
+console.log('hello cac ban')
+console.log('hihihi')
+
+let x =  2 + 3+ "Volvo" + 16 + 4
+let answer1 = "It's alright";
+let answer2 = "He is called 'Johnny'";
+let answer3 = 'He is called "Johnny"';
+console.log(2+ 3+ answer1 + "<br>" + answer2 + "<br>" + answer3 + 2 + 3)
+console.log(2e3) 
+let y = 5;
+let z = 6;
+console.log(y==z)   
+console.log(typeof "John")
+const person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
+console.log(person.firstName  )
+let car = "Volvo";
+car = undefined;
+console.log(answer1.length)
+let text = "We are the so-called \"Vikings\" from the north and 'abc'";
+console.log(text)
